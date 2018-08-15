02b392d0818dc17f9c1e1e8aa5ff32c5cd6f8279
Juan Sebastian Puerta Ordonez <1007672072@academico.icesi.edu.co>
2018-08-14 20:06:26 -0500

    Methods of the class NumberGenerator

--- Project/src/model/NumberGenerator.java
+++ Project/src/model/NumberGenerator.java
@@ -2,4 +2,20 @@ package model;
 
 public class NumberGenerator {
 
+	public int[] orderArray() {
+		return null;
+	}
+	
+	public int[] neatlyOrderArray() {
+		return null;
+	}
+	
+	public int[] randomArray() {
+		return null;
+	}
+	
+	public int[] percentageArray(double percentage) {
+		return null;
+	}
+	
 }
