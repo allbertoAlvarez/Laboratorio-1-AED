2f75687b3a97aedecb6a91d6152357f424d9cae0
Juan David Campillo Cepeda <38546862+KingSatur@users.noreply.github.com>
2018-08-17 16:29:17 -0500

    Project updated

---
+++ Projec/src/model/NumberGenerator.java
@@ -0,0 +1,21 @@
+package model;
+
+public class NumberGenerator {
+
+	public int[] orderArray() {
+		return null;
+	}
+
+	public int[] neatlyOrderArray() {
+		return null;
+	}
+
+	public int[] randomArray() {
+		return null;
+	}
+
+	public int[] percentageArray(double percentage) {
+		return null;
+	}
+
+}
---
+++ Projec/src/model/Test.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Test {
+
+	private NumberGenerator generator;
+	private SortNumbers sort;
+
+	public Test() {
+
+	}
+
+}
