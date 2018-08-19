4a57328b850ab1e966f759844033662d89ecb4e6
Juan David Campillo Cepeda <38546862+KingSatur@users.noreply.github.com>
2018-08-19 11:53:27 -0500

    Changes in interface

--- Projec/src/application/ControllerFirstScene.java
+++ Projec/src/application/ControllerFirstScene.java
@@ -1,19 +1,81 @@
 package application;
 
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class ControllerFirstScene {
 
     @FXML
     private Button randomNumbers;
-	
-	
-	
+    
     @FXML
-    void showMessage(ActionEvent event) {
-    	System.out.println("Hola mundo");
+    private Button randomInverse;
+
+    @FXML
+    private Button randomOrder;
+
+    @FXML
+    private Button randomPercentage;
+    
+    @FXML
+    private TextField size;
+    
+    @FXML
+    private ComboBox<?> sortAlgoritms;
+    
+    @FXML
+    private TextField complexityTemp;
+
+    @FXML
+    void generateRandomInOrder(ActionEvent event) {
+    	String a = JOptionPane.showInputDialog("Introduce la cantidad de numeros: ");
+    	if(a == null || a.equals("") || Integer.parseInt(a) == 0) {
+    		JOptionPane.showMessageDialog(null, "You do not introduce valid value", null, JOptionPane.ERROR_MESSAGE);
+    	}
+    	else {
+    		size.setText(a);
+    	}
+    }
+
+    @FXML
+    void generateRandomInverse(ActionEvent event) {
+    	String a = JOptionPane.showInputDialog("Introduce la cantidad de numeros: ");
+    	if(a == null || a.equals("") || Integer.parseInt(a) == 0 ) {
+    		JOptionPane.showMessageDialog(null, "You do not introduce valid value", null, JOptionPane.ERROR_MESSAGE);
+    	}
+    	else {
+    		size.setText(a);
+    		
+    	}
+
+    }
+
+    @FXML
+    void generateRandomPercentage(ActionEvent event) {
+    	String a = JOptionPane.showInputDialog("Introduce la cantidad de numeros: ");
+    	if(a == null || a.equals("") || Integer.parseInt(a) == 0) {
+    		JOptionPane.showMessageDialog(null, "You do not introduce valid value", null, JOptionPane.ERROR_MESSAGE);
+    	}
+    	else {
+    		size.setText(a);
+    	}
+    }
+		
+    @FXML
+    void generateRandom(ActionEvent event) {
+    	String a = JOptionPane.showInputDialog("Introduce la cantidad de numeros: ");
+    	if(a == null || a.equals("") || Integer.parseInt(a) == 0) {
+    		JOptionPane.showMessageDialog(null, "You do not introduce valid value", null, JOptionPane.ERROR_MESSAGE);
+    	}
+    	else {
+    		size.setText(a);
+    	}
     }
 
 	
