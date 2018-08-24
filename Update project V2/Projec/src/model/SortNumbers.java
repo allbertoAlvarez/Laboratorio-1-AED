7b9f10848034c607822eb5be82745d9f9ac0d522
Juan-Puerta <42070035+Juan-Puerta@users.noreply.github.com>
2018-08-23 23:25:22 -0500

    Merge method and unit tests

--- Update project V2/Projec/src/model/SortNumbers.java
+++ Update project V2/Projec/src/model/SortNumbers.java
@@ -72,6 +72,48 @@ public class SortNumbers {
 	     return arr;
 	}
 	
+	public int[] mergeSort(int arr[], int n) {
+
+		// For current size of subarrays to
+		// be merged curr_size varies from
+		// 1 to n/2
+		int curr_size;
+
+		// For picking starting index of
+		// left subarray to be merged
+		int left_start;
+
+		// Merge subarrays in bottom up
+		// manner. First merge subarrays
+		// of size 1 to create sorted
+		// subarrays of size 2, then merge
+		// subarrays of size 2 to create
+		// sorted subarrays of size 4, and
+		// so on.
+		for (curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) {
+
+			// Pick starting point of different
+			// subarrays of current size
+			for (left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
+				// Find ending point of left
+				// subarray. mid+1 is starting
+				// point of right
+				int mid = left_start + curr_size - 1;
+
+				int right_end = Math.min(left_start + 2 * curr_size - 1, n - 1);
+
+				if (mid > arr.length - 1) {
+					mid = left_start + ((arr.length - left_start) / 2);
+				}
+
+				// Merge Subarrays arr[left_start...mid]
+				// & arr[mid+1...right_end]
+				merge(arr, left_start, mid, right_end);
+			}
+		}
+		return arr;
+	}
+	
 	public void merge(int arr[], int l, int m, int r) {
 		int i, j, k;
 		int n1 = m - l + 1;
